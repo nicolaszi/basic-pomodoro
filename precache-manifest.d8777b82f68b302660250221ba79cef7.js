25bb0f6fd63f79465fa1e05dde83328be57810a8
nicolas <nicolas.zilbermann@gmail.com>
2021-08-01 16:53:45 +0200

    chore: deployment github-pages config

---
+++ precache-manifest.d8777b82f68b302660250221ba79cef7.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e11ca748c6973cec3ff",
+    "url": "/basic-pomodoro/css/app.75298121.css"
+  },
+  {
+    "revision": "900e9b43a770ab47b4c8",
+    "url": "/basic-pomodoro/css/chunk-vendors.77fc7fd4.css"
+  },
+  {
+    "revision": "7cab84550129e0353d1589f134ea2a28",
+    "url": "/basic-pomodoro/endtimer.mp3"
+  },
+  {
+    "revision": "099a9556e1a63ece24f8a99859c94c7d",
+    "url": "/basic-pomodoro/fonts/fa-brands-400.099a9556.woff"
+  },
+  {
+    "revision": "30cc681d4487d2f561035ba24a68c629",
+    "url": "/basic-pomodoro/fonts/fa-brands-400.30cc681d.eot"
+  },
+  {
+    "revision": "3b89dd103490708d19a95adcae52210e",
+    "url": "/basic-pomodoro/fonts/fa-brands-400.3b89dd10.ttf"
+  },
+  {
+    "revision": "f7307680c7fe85959f3ecf122493ea7d",
+    "url": "/basic-pomodoro/fonts/fa-brands-400.f7307680.woff2"
+  },
+  {
+    "revision": "1f77739ca9ff2188b539c36f30ffa2be",
+    "url": "/basic-pomodoro/fonts/fa-regular-400.1f77739c.ttf"
+  },
+  {
+    "revision": "7124eb50fc8227c78269f2d995637ff5",
+    "url": "/basic-pomodoro/fonts/fa-regular-400.7124eb50.woff"
+  },
+  {
+    "revision": "7630483dd4b0c48639d2ac54a894b450",
+    "url": "/basic-pomodoro/fonts/fa-regular-400.7630483d.eot"
+  },
+  {
+    "revision": "f0f8230116992e521526097a28f54066",
+    "url": "/basic-pomodoro/fonts/fa-regular-400.f0f82301.woff2"
+  },
+  {
+    "revision": "1042e8ca1ce821518a2d3e7055410839",
+    "url": "/basic-pomodoro/fonts/fa-solid-900.1042e8ca.eot"
+  },
+  {
+    "revision": "605ed7926cf39a2ad5ec2d1f9d391d3d",
+    "url": "/basic-pomodoro/fonts/fa-solid-900.605ed792.ttf"
+  },
+  {
+    "revision": "9fe5a17c8ab036d20e6c5ba3fd2ac511",
+    "url": "/basic-pomodoro/fonts/fa-solid-900.9fe5a17c.woff"
+  },
+  {
+    "revision": "e8a427e15cc502bef99cfd722b37ea98",
+    "url": "/basic-pomodoro/fonts/fa-solid-900.e8a427e1.woff2"
+  },
+  {
+    "revision": "ba7ed552362f64d30f6d844974d89114",
+    "url": "/basic-pomodoro/img/fa-brands-400.ba7ed552.svg"
+  },
+  {
+    "revision": "0bb428459c8ecfa61b22a03def1706e6",
+    "url": "/basic-pomodoro/img/fa-regular-400.0bb42845.svg"
+  },
+  {
+    "revision": "376c1f97f6553dea1ca9b3f9081889bd",
+    "url": "/basic-pomodoro/img/fa-solid-900.376c1f97.svg"
+  },
+  {
+    "revision": "af27add2c33d5728300ad6e4a457fe8e",
+    "url": "/basic-pomodoro/index.html"
+  },
+  {
+    "revision": "31ffaae9cb6b68bbbbd1",
+    "url": "/basic-pomodoro/js/about.3d976e9d.js"
+  },
+  {
+    "revision": "2e11ca748c6973cec3ff",
+    "url": "/basic-pomodoro/js/app.83009681.js"
+  },
+  {
+    "revision": "900e9b43a770ab47b4c8",
+    "url": "/basic-pomodoro/js/chunk-vendors.55cebde3.js"
+  },
+  {
+    "revision": "81972408b5260b82f2362c91f2f418bb",
+    "url": "/basic-pomodoro/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/basic-pomodoro/robots.txt"
+  }
+]);
